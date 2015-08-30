c8a20128d73624d05c2ceffc3d400416bd9d7362
Divij Bindlish <dvjbndlsh93@gmail.com>
2015-08-30 19:19:15 +0530

    Add functionality to recursively rename directories

--- movienamer/main.py
+++ movienamer/main.py
@@ -1,24 +1,29 @@
 import argparse
+import fnmatch
 import os
+import sys
 
 from identify import identify
 from confirm import confirm
+from keywords import subtitle_extensions, video_extensions
 
-
-def movienamer(movie, options=dict()):
+def movienamer(movie):
     directory = '/'.join(movie.split('/')[:-1])
     filename, extension = os.path.splitext(os.path.basename(movie))
 
     results = identify(filename, directory)
     if len(results) == 0:
-        raise Exception
+        print 'No results found. Skipping movie file'
+        return False
 
     action = confirm(results, filename, extension)
 
     if action == 'SKIP':
-        print 'Skipping movie file\n'
+        print 'Skipping movie file'
+        return False
     elif action == 'QUIT':
         print 'Exiting movienamer'
+        sys.exit()
     else:
         i = int(action)
         result = results[i-1]
@@ -30,20 +35,80 @@ def movienamer(movie, options=dict()):
                 result['title'] +
                 ' [' + result['year'] + ']' +
                 extension)
-        # os.rename(movie, dest)
+
+        if os.path.isfile(dest):
+            print 'File already exists: ' + dest
+            print 'Overwrite?'
+            final_confirmation = raw_input('([y]/n/q)').lower()
+            if final_confirmation == '':
+                final_confirmation = 'y'
+
+            if final_confirmation not in ['y', 'n', 'q']:
+                final_confirmation = raw_input('([y]/n/q)').lower()
+                if final_confirmation == '':
+                    final_confirmation = 'y'
+
+            if final_confirmation == 'n':
+                print 'Skipping movie file\n'
+                return False
+            elif final_confirmation == 'q':
+                print 'Exiting movienamer'
+                sys.exit()
+
+        return movie, dest
 
 
 def main():
     parser = argparse.ArgumentParser(
-        description='Command-line utlity to properly organize movies.'
+        description='Command-line utlity to organize movies.'
     )
 
-    parser.add_argument('movies', nargs='+', help='movie files to rename')
+    parser.add_argument('movie', nargs='+', help='movie files to rename')
+    parser.add_argument('-r', '--recursive', action='store_true',
+                        help='recursively rename movies in deirectories')
 
     args = vars(parser.parse_args())
 
     if len(args) == 0:
         raise Exception
 
-    for movie in args['movies']:
-        movienamer(movie)
+    movies = []
+    errors = []
+
+    if args['recursive']:
+        for movie in args['movie']:
+            if os.path.isfile(movie):
+                movies.append(movie)
+                continue
+            elif not os.path.isdir(movie):
+                errors.append(movie)
+                continue
+
+            for root, dirnames, files in os.walk(movie):
+                for filename in files:
+                    _, extension = os.path.splitext(filename)
+                    if extension in video_extensions:
+                        movies.append(root + '/' + filename)
+
+    else:
+        for filename in args['movie']:
+            _, extension = os.path.splitext(filename)
+            if extension in video_extensions:
+                movies.append(root + '/' + filename)
+            else:
+                errors.append(filename)
+
+    for i, movie in enumerate(movies):
+        result = movienamer(movie)
+        if result == False:
+            errors.append(movie)
+        else:
+            os.rename(*result)
+            print 'Movie succesfully renamed'
+            if i + 1 < len(movies):
+                print ''
+
+    if len(errors) > 0:
+        print 'Unable to rename the following movie files:'
+        for i, filename in enumerate(errors):
+            print '%d: %s' % (i+1, filename)
